f334e6f0ba8499b0b38e6817ea0ae171fe733302
neelaksh <Neelaksh.gupta05@gmail.com>
2026-02-26 19:33:31 -0500

    fixed singleton bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
 //we need to check if a PrismaClient exist
 //Next.js suport hot reloading of changing files, if the framework restarts, we have to ensure that extra unwanted instances of the database do not exist
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-const prisma = globalForPrisma.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  })
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
 
