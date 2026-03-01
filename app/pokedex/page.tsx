07980c3ae20be04808dbc4704764138e737e469a
neelaksh <Neelaksh.gupta05@gmail.com>
2026-03-01 14:27:50 -0500

    add Pokédex page with API data loading and Zustand state initialization

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,14 +1,37 @@
-import { getPokemon } from "@/lib/pokeapi"
+"use client"
 
-export default async function PokedexPage() {
+import { useEffect } from "react"
+import { usePokedexStore } from "@/lib/pokedexStore"
 
-  const pokemon = await getPokemon()
+import Header from "@/components/pokemon/Header"
+import PokemonGrid from "@/components/pokemon/PokemonGrid"
+import Modal from "@/components/pokemon/Modal"
+import SideNav from "@/components/pokemon/SideNav"
+import {getPokemonList} from "@/lib/pokeapi"
+
+export default function PokedexPage() {
+
+  const setPokemon = usePokedexStore(state => state.setPokemon)
+
+  // 🧠 this runs ONCE when page loads
+  useEffect(() => {
+
+    async function loadData() {
+      const pokemon = await getPokemonList()
+      setPokemon(pokemon)
+    }
+
+      loadData()
+    }, [])
 
   return (
-    <div>
-      {pokemon.map((p: any) => (
-        <p key={p.name}>{p.name}</p>
-      ))}
+    <div className="flex">
+      <SideNav />
+      <div className="flex-1">
+        <Header />
+        <PokemonGrid />
+      </div>
+      <Modal />
     </div>
   )
 }
